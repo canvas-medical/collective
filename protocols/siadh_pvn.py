3495ca3ae981cc7c93ec6742f35cebfcefe7c53a
William Burke <williamtburke311@gmail.com>
2024-03-11 14:16:42 -0600

    updating the value set

--- protocols/siadh_pvn.py
+++ protocols/siadh_pvn.py
@@ -11,12 +11,68 @@ from canvas_workflow_kit.value_set.value_set import ValueSet
 from canvas_workflow_kit.timeframe import Timeframe
 
 class TSSurgery(ValueSet):
-    ICD10PCS = {}
-    ICD9CM = {}
+    ICD10PCS = {
+        '0BBH0ZZ',
+        '0BBH3ZZ',
+        '0BBH4ZZ',
+        '0BBH8ZZ',
+        '0BBJ0ZZ',
+        '0BBJ3ZZ',
+        '0BBJ4ZZ',
+        '0BBJ8ZZ'
+    }
+
+    ICD9CM = {
+        '07.61',
+        '07.62',
+        '07.63',
+        '07.64',
+        '07.65',
+        '07.68',
+        '07.69'
+    }
+
+    SNOMEDCT = {
+        '46296006',
+        '53158000',
+        '67598004',
+        '276274008',
+        '446813000',
+        '446814006',
+        '726755001',
+        '726756000'
+    }
+
+    CPT = {
+        '62165',
+        '61548'
+    }
 
 class FluidRestriction(ValueSet):
     VALUE_SET_NAME = "Fluid Restriction"
-    ICD10CM = {}
+    ICD10PCS = {
+        '4A0935Z',  # Measurement of Fluid Balance, Monitoring, External Approach
+        '4A0945Z',  # Measurement of Fluid Balance, Monitoring, Internal Approach
+        '4A0955Z',  # Measurement of Fluid Balance, Monitoring, Circulatory Approach
+        '4A0965Z'  # Measurement of Fluid Balance, Monitoring, Respiratory Approach
+    }
+
+    ICD9CM = {
+        '25.82'  # Fluid restriction
+    }
+
+    SNOMEDCT = {
+        '61426000',  # Fluid intake restriction
+        '226355009',  # Restriction of fluid intake
+        '226358006',  # Fluid balance management
+        '243807004',  # Strict fluid balance management
+        '274411005',  # Fluid restriction education
+        '286024002',  # Monitoring of fluid input/output
+        '361231003',  # Fluid restriction therapy
+        '443288003',  # Fluid balance education
+        '710742001',  # Monitoring of fluid restriction
+        '710743006'  # Fluid restriction regimen
+    }
 
 class MyFirstProtocol(ClinicalQualityMeasure):
 
