4f6543f29406cb1e1126f01ddbe55593b4183def
dmhardin102 <32660553+dmhardin102@users.noreply.github.com>
2024-03-11 22:43:26 -0500

    Update AnkleXR.py
    wokring version 2.0

--- protocols/AnkleXR.py
+++ protocols/AnkleXR.py
@@ -6,17 +6,18 @@ from canvas_workflow_kit.protocol import (
 )
 
 from canvas_workflow_kit.constants import CHANGE_TYPE
-from canvas_workflow_kit.recommendation import Recommendation, ImagingRecommendation
+from canvas_workflow_kit.recommendation import InstructionRecommendation, ImagingRecommendation
 from canvas_workflow_kit.value_set import ValueSet
 
 
 class AnkleInjury(ValueSet):
     VALUE_SET_NAME = 'Ankle Injury'
-    SNOMEDCT = {'125603006'}
-    ICD10CM = {'S93.402A'}
+    SNOMEDCT = {'404684003'}
+    ICD10CM = {'S93402A'}
 
 class AnkleXR2V(ValueSet):
     """
+
     Clinical Focus:
     Data Element Scope:
     Inclusion Criteria:
@@ -31,6 +32,11 @@ class AnkleXR2V(ValueSet):
 
     SNOMEDCT = {'19490002'}
 
+class AnkleInjuryInstructions(ValueSet):
+
+    VALUE_SET_NAME = 'Ankle Injury Rehab'
+   
+    SNOMEDCT = {'229191008'}
 
 class AnkleInjuryXR(ClinicalQualityMeasure):
 
@@ -40,7 +46,7 @@ class AnkleInjuryXR(ClinicalQualityMeasure):
 
         description = 'Protocol to identify patients with ankle injuries who have should recieve an X-Ray'
 
-        version = '2024-03-11v1.94test'
+        version = '2024-03-11v2.0'
 
         information = 'https://docs.canvasmedical.com'
 
@@ -50,6 +56,7 @@ class AnkleInjuryXR(ClinicalQualityMeasure):
 
         compute_on_change_types = [
             CHANGE_TYPE.CONDITION,
+            CHANGE_TYPE.INSTRUCTION
         ]
             
 
@@ -63,14 +70,12 @@ class AnkleInjuryXR(ClinicalQualityMeasure):
         Patients with diagnosis of Ankle Injury
         """
         return (len(self.patient.conditions.find(AnkleInjury))>0)
-        
+
     def in_numerator(self):
         """
         Have patients been X-rayed or not.
         """
-        return (len(self.patient.tasks.find(AnkleInjuryXR))>0) 
-        
-
+        return (len(self.patient.instructions.find(AnkleInjuryInstructions))>0) 
 
     def compute_results(self):
         result = ProtocolResult()
@@ -86,13 +91,25 @@ class AnkleInjuryXR(ClinicalQualityMeasure):
                 result.due_in = -1
                 result.add_narrative(f'If {self.patient.first_name} has pain in areas of Ottawa rules, then he/she may need an Ankle XR')
 
-                result.add_recommendation(
-                    ImagingRecommendation(
-                        key='MSK-ankleinjuryXR',
-                        rank=1,
-                        button='Order Ankle X-Ray',
-                        patient=self.patient,
-                        imaging=AnkleXR2V,
-                        title='Order Ankle X-Ray(2-views)'))
-                       
+                ankle_imaging=ImagingRecommendation(
+                    key='MSK-ankleinjuryXR',
+                    rank=2,
+                    button='Order Ankle X-Ray',
+                    patient=self.patient,
+                    imaging=AnkleXR2V,
+                    title='Order Ankle X-Ray(2-views)'
+                    )
+                
+                ankle_rehab_instructions=InstructionRecommendation(
+                    key='ANKLE_REHAB_INSTRUCTIONS',
+                    rank=1,
+                    patient=self.patient,
+                    instruction=AnkleInjuryInstructions,
+                    title='Educate and Order XR Below',
+                    narrative='Patient to follow ankle rehab program and proceed with Ankle XR'
+                    )
+                
+                result.add_recommendation(ankle_rehab_instructions)
+                result.add_recommendation(ankle_imaging)
+
         return result
